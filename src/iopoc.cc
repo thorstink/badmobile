1252adeb931d58a2147fdf5af7a25869e1da802c
Thomas Horstink <thomashorstink@gmail.com>
2020-01-30 20:20:57 +0100

    blink led on twist

--- src/iopoc.cc
+++ src/iopoc.cc
@@ -6,34 +6,21 @@
 
 // https://github.com/aquaticus/nexus433/blob/master/CMakeLists.txt
 
-int main(int argc, char **argv) {
-  if (argc < 3) {
-    ::std::cerr << "usage: " << argv[0] << " <chip> <line_offset0>=<value0> ..."
-                << ::std::endl;
-    return EXIT_FAILURE;
-  }
+int main(int, char **) {
+
+  auto chip_nr = "0";
+  auto gpio_nr = "14";
 
   ::std::vector<unsigned int> offsets;
   ::std::vector<int> values;
-  for (int i = 2; i < argc; i++) {
-    ::std::string arg(argv[i]);
 
-    size_t pos = arg.find('=');
+  values.push_back(::std::stoul(gpio_nr));
+  offsets.push_back(::std::stoul(gpio_nr));
 
-    ::std::string offset(arg.substr(0, pos));
-    ::std::string value(arg.substr(pos + 1, ::std::string::npos));
-
-    if (offset.empty() || value.empty())
-      throw ::std::invalid_argument("invalid argument: " +
-                                    ::std::string(argv[i]));
-
-    offsets.push_back(::std::stoul(offset));
-    values.push_back(::std::stoul(value));
-  }
-  ::gpiod::chip chip(argv[1]);
+  ::gpiod::chip chip(chip_nr);
   auto lines = chip.get_lines(offsets);
 
-  lines.request({argv[0], ::gpiod::line_request::DIRECTION_OUTPUT, 0}, values);
+  lines.request({chip_nr, ::gpiod::line_request::DIRECTION_OUTPUT, 0}, values);
   int toggle = 1;
   for (;;) {
     toggle == 0 ? toggle = 1 : toggle = 0;
