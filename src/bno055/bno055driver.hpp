3ac145e05bf6e2216518cb1fa4b82b26be6d71e5
Thomas Horstink <thomashorstink@gmail.com>
2020-02-09 00:44:00 +0100

    interface seems to be ok

--- src/bno055/bno055driver.hpp
+++ src/bno055/bno055driver.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 extern "C" {
-// because it is a c-library
 #include "getbno055.h"
 }
 #include <rxcpp/rx.hpp>
@@ -9,25 +8,45 @@ extern "C" {
 char senaddr[256] = "0x28";
 
 struct imu_t {
-  uint64_t t;
+  int64_t t;
   double linear_acceleration_x, linear_acceleration_y, linear_acceleration_z;
   double angular_velocity_x, angular_velocity_y, angular_velocity_z;
 };
 
 inline rxcpp::observable<imu_t> createImuObservable() {
   return rxcpp::observable<>::create<imu_t>([](rxcpp::subscriber<imu_t> s) {
-
     get_i2cbus(senaddr);
-    
-    auto res = bno_dump();
-    if(res != 0) {
-        printf("Error: could not dump the register maps.\n");
+
+    constexpr auto new_mode = opmode_t::accgyro;
+    if (set_mode(new_mode) != 0) {
+      printf("Error: could not set sensor mode [0x%02X].\n", new_mode);
+      s.on_error(std::exception_ptr());
     }
 
-    for (int i = 0; i < 5; ++i) {
+    std::chrono::time_point<std::chrono::steady_clock> now;
+    std::chrono::time_point<std::chrono::steady_clock> wakeup_time;
+    constexpr auto delta = std::chrono::milliseconds(5);
+
+    for (;;) {
+      now = std::chrono::steady_clock::now();
       if (!s.is_subscribed())
         break;
-      s.on_next(imu_t());
+
+      struct bnoacc acc;
+      struct bnogyr gyr;
+      const auto acc_ok = get_acc(&acc);
+      const auto gyro_ok = get_gyr(&gyr);
+
+      (!gyro_ok && !acc_ok)
+          ? s.on_next(imu_t{now.time_since_epoch().count(), acc.adata_x,
+                            acc.adata_y, acc.adata_z, gyr.gdata_x, gyr.gdata_y,
+                            gyr.gdata_z})
+          : s.on_error(std::exception_ptr());
+
+      wakeup_time = now + delta;
+      if (wakeup_time > std::chrono::steady_clock::now()) {
+        std::this_thread::sleep_until(wakeup_time);
+      }
     }
     s.on_completed();
   });
--- src/imu_test.cc
+++ src/imu_test.cc
@@ -2,5 +2,12 @@
 
 int main() {
   auto imu = createImuObservable();
-  imu.subscribe();
+  imu.subscribe([](const auto &imu_msg) {
+    std::cout << imu_msg.t << ", " << imu_msg.linear_acceleration_x << ", "
+              << imu_msg.linear_acceleration_y << ", "
+              << imu_msg.linear_acceleration_z << ", "
+              << imu_msg.angular_velocity_x << ", "
+              << imu_msg.angular_velocity_y << ", "
+              << imu_msg.angular_velocity_z << std::endl;
+  });
 }
