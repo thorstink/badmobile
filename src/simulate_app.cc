1dc3fcc983f1149e9af84629fd776e61982c682c
Thomas Horstink <thomashorstink@gmail.com>
2020-02-26 23:20:44 +0100

    undo

--- src/simulate_app.cc
+++ src/simulate_app.cc
@@ -9,18 +9,15 @@ int main() {
 
   const auto cmd_vel_handle = std::make_shared<CmdVelHandler>([] {});
   const auto imu_handle = std::make_shared<ImuHandler>();
-  auto fake_imu = createFakeImu().publish();
+  const auto fake_imu = createFakeImu();
 
-  fake_imu.map(&to_json).subscribe([&](const auto &j) {
+  const auto t = rxcpp::observe_on_new_thread();
+  fake_imu.map(&to_json).subscribe_on(t).subscribe([&](const auto &j) {
     server.execute([imu_handle, j]() { imu_handle->send(j); });
   });
 
   server.addWebSocketHandler("/cmd", cmd_vel_handle);
   server.addWebSocketHandler("/imu", imu_handle);
-  auto j = std::thread([&server] { server.serve("ui", 2222); });
-
-  fake_imu.connect();
-
-  j.join();
+  server.serve("ui", 2222);
   return 0;
 }
