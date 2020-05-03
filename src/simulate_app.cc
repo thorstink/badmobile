53967a140120a16007e780e382136a7f00e7311a
Thomas <thomashorstink@gmail.com>
2020-05-03 15:46:26 +0200

    with fmt (#6)
    * with fmt
    
    * build pipe
    
    * build pipe

--- src/simulate_app.cc
+++ src/simulate_app.cc
@@ -1,9 +1,23 @@
 #include "cmd_vel_socket.hpp"
 #include "imu_socket.hpp"
+#include <fmt/format.h>
 #include <seasocks/PrintfLogger.h>
 #include <seasocks/Server.h>
 
-int main() {
+int main(int argc, const char *argv[]) {
+
+  auto command = std::string{};
+  for (auto cursor = argv, end = argv + argc; cursor != end; ++cursor) {
+    command += std::string{*cursor};
+  }
+
+  std::cerr << "command = " << command.c_str() << std::endl;
+
+  fmt::print("Running application {0} using {1} for configuration.", argv[0],
+             argv[1]);
+  // manually flush because otherwise not visable.
+  std::cout.flush();
+
   seasocks::Server server(
       std::make_shared<seasocks::PrintfLogger>(seasocks::Logger::Level::Error));
 
@@ -19,6 +33,7 @@ int main() {
 
   server.addWebSocketHandler("/cmd", cmd_vel_handle);
   server.addWebSocketHandler("/imu", imu_handle);
+
   server.serve("ui", 2222);
   return 0;
 }
