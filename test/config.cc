f62c762f29471201d65fb76953f8d32e30b13d8f
Thomas Horstink <thomashorstink@gmail.com>
2020-05-11 17:39:25 +0200

    die evil json

--- test/config.cc
+++ test/config.cc
@@ -1,29 +1,33 @@
 #include "lsm9ds1/config.hpp"
 #include <catch2/catch.hpp>
+#include <iostream>
 
 TEST_CASE("Test imu config") {
   // for now like this.
-  nlohmann::json settings = {
-      {"robot",
-       {"name",
-        "The Badmobile",
-        "dimensions",
-        {"track", 0.2, "wheel_diameter_left", 0.05, "wheel_diameter_right",
-         0.05},
-        "hardware",
-        {"imu",
-         {"sampling_frequency", 10, "low_pass_cut_off_frequency", 100, "SCLK",
-          11, "MOSI", 10, "MISO", 9, "CSAG", 5},
-         "motor_left",
-         {"PWM_GPIO_FORWARD", 10, "PWM_GPIO_REVERSE", 11},
-         "motor_right",
-         {"PWM_GPIO_FORWARD", 12, "PWM_GPIO_REVERSE", 13},
-         "led",
-         {"GPIO", 12}}}}};
+  nlohmann::json settings;
+  settings["robot"]["name"] = "The Badmobile";
+  settings["robot"]["differential_drive_parameters"]["track"] = 0.2;
+  settings["robot"]["differential_drive_parameters"]["wheel_diameter_left"] =
+      0.5;
+  settings["robot"]["differential_drive_parameters"]["wheel_diameter_right"] =
+      0.5;
+  settings["robot"]["hardware"]["imu"]["sampling_frequency"] = 10;
+  settings["robot"]["hardware"]["imu"]["low_pass_cut_off_frequency"] = 100;
+  settings["robot"]["hardware"]["imu"]["SCLK"] = 11;
+  settings["robot"]["hardware"]["imu"]["MOSI"] = 10;
+  settings["robot"]["hardware"]["imu"]["MISO"] = 9;
+  settings["robot"]["hardware"]["imu"]["CSAG"] = 5;
+  settings["robot"]["hardware"]["motor_left"]["PWM_GPIO_FORWARD"] = 10;
+  settings["robot"]["hardware"]["motor_left"]["PWM_GPIO_REVERSE"] = 11;
+  settings["robot"]["hardware"]["motor_right"]["PWM_GPIO_FORWARD"] = 12;
+  settings["robot"]["hardware"]["motor_right"]["PWM_GPIO_REVERSE"] = 13;
+  settings["robot"]["hardware"]["led"]["GPIO"] = 12;
+
   SECTION("contains imu config") {
     REQUIRE(lsm9ds1::containsImuConfig(settings));
   }
   SECTION("imu config is valid") {
-    REQUIRE(lsm9ds1::containsImuConfig(settings["robot"]["hardware"]["imu"]));
+    const auto &imu_config = settings["robot"]["hardware"]["imu"];
+    REQUIRE(lsm9ds1::imuConfigIsValid(imu_config));
   }
 }
